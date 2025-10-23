df64f35630f9426f71ca4fe5da75e972426ab302
engy <esadik@student.ubc.ca>
2025-10-22 19:02:17 -0700

    fixing directory for serviceAccountKey.json

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../serviceAccountKey.json'; //have to generate it for yourslef
+import serviceAccount from '../../serviceAccountKey.json'; //have to generate it for yourslef
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
